4648a714b48bea0e354ac524f1b826e749169c5b
송은혜 <dmsgp8292@gmail.com>
2024-05-14 18:05:52 +0900

    runnable 을 이용한 스레드 구현

--- src/example2/Main.java
+++ src/example2/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Thread thread = new NewThread();
         thread.start();
+
+        Thread runnableThread = new Thread(new RunnableThread());
+        runnableThread.start();
     }
 
     //상속을 이용하여 여러 스레드 구현을 간단하게 만들자.
@@ -11,6 +14,13 @@ public class Main {
         @Override
         public void run() {
             System.out.println("Hello " + this.getName());
+        }
+    }
+
+    public static class RunnableThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Hello! It's Runnable Thread.");
         }
     }
 }
