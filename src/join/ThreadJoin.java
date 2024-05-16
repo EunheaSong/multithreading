2a3c1df4d9bc81f4f436139cba7c990ff506149f
송은혜 <dmsgp8292@gmail.com>
2024-05-16 17:42:16 +0900

    Thread join _ 주석 추가

--- src/join/ThreadJoin.java
+++ src/join/ThreadJoin.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 public class ThreadJoin {
     public static void main(String[] args) throws InterruptedException {
-        List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 4656L, 23L, 2435L, 5566L);
-
+        List<Long> inputNumbers = Arrays.asList(10000000L, 3435L, 35435L, 4656L, 23L, 2435L, 5566L);
         List<FactorialThread> threads = new ArrayList<>();
 
         for (long inputNumber : inputNumbers) {
             threads.add(new FactorialThread(inputNumber));
         }
         for (Thread thread : threads) {
+            //긴 작업의 경우, 메인 스레드의 작업이 종료되어도 해당 스레드 때문에 애플리케이션이 종료되지 않는 것을 방지.
             thread.setDaemon(true);
             thread.start();
         }
         for (Thread thread : threads) {
+            //스레드의 반환 값을 받아서 메인 스레드의 작업을 수행하기 위해 2초간 메인 스레드가 반환 값을 기다리도록 한다.
+            //인자를 넣어주지 않으면 스레드의 작업이 끝날 때까지 무한히 대기함.
             thread.join(2000);
         }
+
         for (int i = 0; i < inputNumbers.size(); i++) {
             FactorialThread factorialThread = threads.get(i);
             if (factorialThread.isFinished()) {
+                //팩토리얼 계산 스레드의 계산이 끝난 경우.
                 System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
             } else {
+                //팩토리얼 계산 스레드가 아직 계산 중일 경우
                 System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress.");
             }
         }
@@ -42,9 +47,11 @@ public class ThreadJoin {
 
         public BigInteger factorial(long n) {
             BigInteger tempResult = BigInteger.ONE;
+            System.out.println("I'm starting!");
             for (long i = n; i > 0; i--) {
-                result = result.multiply(new BigInteger(Long.toString(i)));
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
             }
+            System.out.println("I'm ended!");
             return tempResult;
         }
 
